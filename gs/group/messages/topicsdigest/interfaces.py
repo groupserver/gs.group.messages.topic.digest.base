9d87c93d8ade5ee2285aab4f3365a5dcceb9536a
wbushey <wbushey@gmail.com>
2012-11-16 12:40:05 -0600

    Working skeleton of send_all_digests form with AuthToken. It authenticates, but doesn't actually send digests.

--- gs/group/messages/topicsdigest/interfaces.py
+++ gs/group/messages/topicsdigest/interfaces.py
@@ -1,5 +1,7 @@
 # coding=utf-8
 from zope.viewlet.interfaces import IViewletManager
+from zope.interface import Interface
+from gs.auth.token import AuthToken
 
 class IDailyTopicsDigestHtmlVM(IViewletManager):
     ''' Viewlet manager for daily topics digests '''
@@ -12,3 +14,10 @@ class IWeeklyTopicsDigestHtmlVM(IViewletManager):
 
 class IWeeklyTopicsDigestTxtVM(IViewletManager):
     ''' Viewlet manager for weekly topics digests '''
+
+class ISendAllDigests(Interface):
+    '''Declares the form that will be used to send digests for all of the groups on the site. '''
+    
+    token = AuthToken(title=u'Token',
+                      description=u'The authentication token',
+                      required=True)
---
+++ gs/group/messages/topicsdigest/sendDigests.py
@@ -0,0 +1,31 @@
+# coding=utf-8
+from zope.formlib import form
+from Products.Five.browser.pagetemplatefile import ZopeTwoPageTemplateFile
+from gs.content.form.form import SiteForm
+from gs.auth.token import log_auth_error
+from interfaces import ISendAllDigests
+
+class SendAllDigests(SiteForm):
+    label = u'Send Digests for All Groups on the Site'
+    pageTemplateFileName = 'browser/templates/send_all_digests.pt'
+    template = ZopeTwoPageTemplateFile(pageTemplateFileName)
+    form_fields = form.Fields(ISendAllDigests, render_context=False)
+
+    def setUpWidgets(self, ignore_request=False):
+        data = {'name': self.siteInfo.name,}
+        self.widgets = form.setUpWidgets(
+            self.form_fields, self.prefix, self.context,
+            self.request, form=self, data=data,
+            ignore_request=ignore_request)
+
+    @form.action(label=u'Send All Digests', failure='handle_send_all_digests_failure')
+    def handle_send_all_digests(self, action, data):
+        #Magic!
+        self.status = u'<p>You be authenticated!</p>'
+
+    def handle_send_all_digests_failure(self, action, data, errors):
+        log_auth_error(self.context, self.request, errors)
+        if len(errors) ==1:
+            self.status = u'<p>There is an error:</p>'
+
+        assert type(self.status) == unicode
