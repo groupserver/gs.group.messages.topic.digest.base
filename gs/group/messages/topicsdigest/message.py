39f1d8e645344f787bcf69797f467b8cd8d5619e
Michael JasonSmith <mpj17@onlinegroups.net>
2012-12-14 12:57:42 +1100

    Adding some headers

--- gs/group/messages/topicsdigest/message.py
+++ gs/group/messages/topicsdigest/message.py
@@ -45,18 +45,49 @@ class Message(object):
                                 self.rawFromAddress))
         return retval
 
+    def h(self, h):
+        'Turn the text h into a nice header string'
+        retval = str(Header(h, utf8))
+        return retval
+
     def create_message(self, subject, txtMessage, htmlMessage):
         # Stolen from gs.profile.notify.sender.MessageSender
         container = MIMEMultipart('alternative')
-        container['Subject'] = str(Header(subject, utf8))
+        # Required headers
+        container['Subject'] = self.h(subject)
         container['From'] = self.fromAddress
         container['To'] = self.toAddress
+        # Nice-to-have headers
+        container['Precedence'] = self.h('Bulk')
+        container['Organization'] = self.h(self.siteInfo.name)
+        # User-Agent is not actually a real header, but Mozilla and MS use it.
+        brag = 'GroupServer (gs.groups.messages.topicsdigest)'
+        container['User-Agent'] = self.h(brag)
 
+        # RFC2369 headers: <http://tools.ietf.org/html/rfc2369>
+        p = '<mailto:{0}>'.format(self.rawFromAddress)
+        container['List-Post'] = self.h(p)
+
+        u = '<mailto:{0}?Subject=Unsubscribe>'.format(self.rawFromAddress)
+        container['List-Unsubscribe'] = self.h(u)
+
+        a = '<{0}> (Archive of {1})'.format(self.groupInfo.url,
+                                            self.groupInfo.name)
+        container['List-Archive'] = self.h(a)
+
+        helpS = '<{0}/help> (Help)'.format(self.siteInfo.url)
+        container['List-Help'] = self.h(helpS)
+
+        s = '<mailto:{0}> ({1} Support)'.format(
+                self.siteInfo.get_support_email(), self.siteInfo.name)
+        container['List-Owner'] = self.h(s)
+
+        # Construct the body
         txt = MIMEText(txtMessage.encode(utf8), 'plain', utf8)
         container.attach(txt)
-
         html = MIMEText(htmlMessage.encode(utf8), 'html', utf8)
         container.attach(html)
+
         retval = container.as_string()
         assert retval
         return retval
