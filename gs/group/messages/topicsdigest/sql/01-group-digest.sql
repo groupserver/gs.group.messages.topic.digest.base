3934793e388a76f45b9c6d3760172a7f1fb717ea
Michael JasonSmith <mpj17@onlinegroups.net>
2014-06-04 22:48:35 -0700

    Adding the CREATE TABLES SQL here.

---
+++ gs/group/messages/topicsdigest/sql/01-group-digest.sql
@@ -0,0 +1,9 @@
+SET CLIENT_ENCODING = 'UTF8';
+SET CLIENT_MIN_MESSAGES = WARNING;
+
+-- The table that records when a digest went out to the group members.
+CREATE TABLE group_digest (
+    site_id    TEXT                      NOT NULL,
+    group_id   TEXT                      NOT NULL,
+    sent_date  TIMESTAMP WITH TIME ZONE  NOT NULL DEFAULT NOW()
+);
