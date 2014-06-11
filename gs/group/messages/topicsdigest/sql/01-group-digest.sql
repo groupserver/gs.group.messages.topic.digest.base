8a45608e111e0f1072e69dd48a681e858f71b6d9
Michael JasonSmith <mpj17@onlinegroups.net>
2014-06-11 13:17:14 -0700

    Updating the SQL

--- gs/group/messages/topicsdigest/sql/01-group-digest.sql
+++ gs/group/messages/topicsdigest/sql/01-group-digest.sql
@@ -5,5 +5,6 @@ SET CLIENT_MIN_MESSAGES = WARNING;
 CREATE TABLE group_digest (
     site_id    TEXT                      NOT NULL,
     group_id   TEXT                      NOT NULL,
-    sent_date  TIMESTAMP WITH TIME ZONE  NOT NULL DEFAULT NOW()
+    sent_date  TIMESTAMP WITH TIME ZONE  NOT NULL DEFAULT NOW(),
+    PRIMARY KEY (site_id, group_id, sent_date)
 );
