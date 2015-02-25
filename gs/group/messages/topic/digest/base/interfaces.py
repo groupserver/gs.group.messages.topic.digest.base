2e46f641f6ea69ea53d55c3a38f25f2066cbadcf
Michael JasonSmith <mpj17@onlinegroups.net>
2015-02-24 17:29:37 -0800

    Switching to Unicode literals

--- gs/group/messages/topic/digest/base/interfaces.py
+++ gs/group/messages/topic/digest/base/interfaces.py
@@ -1,4 +1,18 @@
-# coding=utf-8
+# -*- coding: utf-8 -*-
+############################################################################
+#
+# Copyright © 2012, 2013, 2014, 2015 E-Democracy.org and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+############################################################################
+from __future__ import unicode_literals
 from zope.viewlet.interfaces import IViewletManager
 from zope.interface import Interface
 from gs.auth.token import AuthToken
@@ -24,9 +38,10 @@ class ISendAllDigests(Interface):
     '''Declares the form that will be used to send digests for all of the
     groups on the site. '''
 
-    token = AuthToken(title=u'Token',
-                      description=u'The authentication token',
-                      required=True)
+    token = AuthToken(
+        title='Token',
+        description='The authentication token',
+        required=True)
 
 
 class ITopicsDigestNotifier(Interface):
