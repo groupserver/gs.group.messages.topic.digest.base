a3511d5527d2dd890057463f36836b5e3ce4931e
Michael JasonSmith <mpj17@onlinegroups.net>
2015-03-25 14:06:30 -0700

    Adding a few Copyright blocks

--- gs/group/messages/topic/digest/base/sendDigests.py
+++ gs/group/messages/topic/digest/base/sendDigests.py
@@ -1,4 +1,17 @@
 # -*- coding: utf-8 -*-
+############################################################################
+#
+# Copyright © 2012, 2013, 2014, 2015 E-Democracy.org and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+############################################################################
 from __future__ import absolute_import, unicode_literals
 from zope.component import getMultiAdapter
 from zope.component.interfaces import ComponentLookupError
