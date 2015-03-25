35062a84b04710c0445605a95cb3914d9962f579
Michael JasonSmith <mpj17@onlinegroups.net>
2015-03-25 10:49:17 -0700

    Moving some of the interfaces out

--- gs/group/messages/topic/digest/base/interfaces.py
+++ gs/group/messages/topic/digest/base/interfaces.py
@@ -13,25 +13,8 @@
 #
 ############################################################################
 from __future__ import unicode_literals
-from zope.viewlet.interfaces import IViewletManager
 from zope.interface import Interface
 from gs.auth.token import AuthToken
-
-
-class IDailyTopicsDigestHtmlVM(IViewletManager):
-    ''' Viewlet manager for daily topics digests '''
-
-
-class IDailyTopicsDigestTxtVM(IViewletManager):
-    ''' Viewlet manager for daily topics digests '''
-
-
-class IWeeklyTopicsDigestHtmlVM(IViewletManager):
-    ''' Viewlet manager for weekly topics digests '''
-
-
-class IWeeklyTopicsDigestTxtVM(IViewletManager):
-    ''' Viewlet manager for weekly topics digests '''
 
 
 class ISendAllDigests(Interface):
