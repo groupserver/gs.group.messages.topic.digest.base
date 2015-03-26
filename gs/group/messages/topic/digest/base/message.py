2f1857a6282d579d2ed47f6ad9bf63826023dbc7
Michael JasonSmith <mpj17@onlinegroups.net>
2015-03-26 10:48:00 -0700

    Changing the Message class to add more headers, and format addresses better.

--- gs/group/messages/topic/digest/base/message.py
+++ gs/group/messages/topic/digest/base/message.py
@@ -16,7 +16,11 @@ from email.Header import Header
 from email.MIMEText import MIMEText
 from email.MIMEMultipart import MIMEMultipart
 from email.utils import formataddr
-from zope.i18nmessageid import MessageFactory
+try:
+    from urllib import parse as urlparse  # Python 3
+except ImportError:
+    from urlparse import urlparse  # Python 2
+    from zope.i18nmessageid import MessageFactory
 from zope.component import createObject
 from zope.cachedescriptors.property import Lazy
 _ = MessageFactory('groupserver')
@@ -25,8 +29,11 @@ utf8 = 'utf-8'
 
 class Message(object):
     # TODO: Create and use a MessageSender made for notifying groups
-    def __init__(self, group):
+    def __init__(self, group, subject, text, html):
         self.context = self.group = group
+        self.subject = subject
+        self.text = text
+        self.html = html
 
     @Lazy
     def siteInfo(self):
@@ -38,20 +45,22 @@ class Message(object):
     @Lazy
     def groupInfo(self):
         retval = createObject('groupserver.GroupInfo', self.context)
-        assert retval, 'Could not create the GroupInfo from %s' % self.context
+        assert retval, 'Could not create the GroupInfo from '\
+                       '"%s"' % self.context
         return retval
 
     @Lazy
     def mailingListInfo(self):
         retval = createObject('groupserver.MailingListInfo', self.context)
-        assert retval, 'Cound not create the MailingListInfo instance from '\
-            '%s' % self.context
+        assert retval, 'Cound not create the MailingListInfo instance '\
+                       'from "%s"' % self.context
         return retval
 
     @Lazy
     def toAddress(self):
         address = self.mailingListInfo.get_property('mailto')
-        retval = formataddr((self.groupInfo.name, address))
+        n = self.h(self.groupInfo.name)
+        retval = formataddr((n, address))
         return retval
 
     @Lazy
@@ -61,10 +70,12 @@ class Message(object):
 
     @Lazy
     def fromAddress(self):
-        retval = formataddr((self.groupInfo.name, self.rawFromAddress))
+        n = self.h(self.groupInfo.name)
+        retval = formataddr((n, self.rawFromAddress))
         return retval
 
-    def h(self, h):
+    @staticmethod
+    def h(h):
         'Turn the text h into a nice header string'
         retval = str(Header(h, utf8))
         assert retval
@@ -75,11 +86,17 @@ class Message(object):
         container['Subject'] = self.h(subject)
         container['From'] = self.fromAddress
         container['To'] = self.toAddress
+        # Sender?
+        s = formataddr((self.h('{0} Support'.format(self.siteInfo.name)),
+                        self.siteInfo.get_support_email()))
+        container['Sender'] = s
         # Nice-to-have headers
-        container['Precedence'] = 'Bulk'
+        container['Precedence'] = b'Bulk'
         container['Organization'] = self.h(self.siteInfo.name)
-        # User-Agent is not actually a real header, but Mozilla and MS use it.
-        brag = 'GroupServer (gs.groups.messages.topicsdigest)'
+        # User-Agent is not actually a real header, but Mozilla and MS use
+        # it.
+        brag = 'GroupServer  <http://groupserver.org/> '\
+               '(gs.groups.messages.digest.base)'
         container['User-Agent'] = self.h(brag)
 
         # RFC2369 headers: <http://tools.ietf.org/html/rfc2369>
@@ -87,38 +104,46 @@ class Message(object):
             p = '<mailto:{0}>'.format(self.rawFromAddress)
             container['List-Post'] = self.h(p)
 
-            u = '<mailto:{0}?Subject=Unsubscribe>'.format(self.rawFromAddress)
+            u = '<mailto:{0}?Subject=Unsubscribe>'.format(
+                self.rawFromAddress)
             container['List-Unsubscribe'] = self.h(u)
 
-            a = '<{0}> (Archive of {1})'\
-                .format(self.groupInfo.url, self.groupInfo.name.encode(utf8))
+            a = '<{0}>'.format(self.groupInfo.url)
             container['List-Archive'] = self.h(a)
 
             helpS = '<{0}/help> (Help)'.format(self.siteInfo.url)
             container['List-Help'] = self.h(helpS)
 
-            s = '<mailto:{0}> ({1} Support)'.format(
-                self.siteInfo.get_support_email(),
-                self.siteInfo.name.encode(utf8))
+            s = '<mailto:{0}>'.format(self.siteInfo.get_support_email())
             container['List-Owner'] = self.h(s)
+
+            # List-ID <http://tools.ietf.org/html/rfc2919>
+            canonicalHost = urlparse(self.siteInfo.url).netloc
+            gid = '{groupInfo.name} '\
+                '<{groupInfo.id}.{canonicalHost}>'.format(
+                    groupInfo=self.groupInfo, canonicalHost=canonicalHost)
+            container['List-ID'] = gid
         except UnicodeDecodeError:
             # FIXME: Sometimes data is just too messed up.
             # http://farmdev.com/talks/unicode/
             pass
-
         return container
 
-    def create_message(self, subject, txtMessage, htmlMessage):
+    def as_string(self):
         # Stolen from gs.profile.notify.sender.MessageSender
         container = MIMEMultipart('alternative')
-        container = self.add_headers(container, subject)
+        container = self.add_headers(container, self.subject)
 
         # Construct the body
-        txt = MIMEText(txtMessage.encode(utf8), 'plain', utf8)
-        container.attach(txt)
-        html = MIMEText(htmlMessage.encode(utf8), 'html', utf8)
+        text = MIMEText(self.text.encode(utf8), 'plain', utf8)
+        container.attach(text)
+        html = MIMEText(self.html.encode(utf8), 'html', utf8)
         container.attach(html)
 
         retval = container.as_string()
         assert retval
+        return retval
+
+    def __str__(self):
+        retval = self.as_string()
         return retval
